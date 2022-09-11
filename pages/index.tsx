fa71ab25823e7299361381b1cb60a73cccea9e29
Brandon <brandonlimjingzhi@gmail.com>
2022-09-11 18:00:59 +0800

    Added Rubik font.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500 min-h-screen">
-      <p>Hello!</p>
+    <div className="bg-white min-h-screen font-rubik">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis quia ut ea, repellendus laborum dolores facere, repudiandae aliquam laudantium
+        quam, fuga velit tenetur molestias sint magni veritatis deserunt! Repudiandae, inventore.
+      </p>
     </div>
   );
 };
